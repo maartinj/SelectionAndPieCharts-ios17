f95a5f454a8adb3dff4f31eb2ff46982a589ee6b
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2024-05-31 16:55:31 +0200

    feat: Sector selection (completed Selection and PieCharts in iOS 17)

--- SelectionAndPieCharts/DonutChartView.swift
+++ SelectionAndPieCharts/DonutChartView.swift
@@ -21,17 +21,60 @@ struct DonutChartView: View {
                 Chart(wineTypes) { wineType in
                     SectorMark(angle: .value("In Stock", wineType.inStock),
                                innerRadius: .ratio(0.65),
+                               outerRadius: selectedWineType?.name == wineType.name ? 175 : 150,
                                angularInset: 1
                     )
 //                        .foregroundStyle(by: .value("Wine Type", wineType.name))
                         .foregroundStyle(wineType.color)
                         .cornerRadius(10)
                 }
+                .chartAngleSelection(value: $selectedCount)
+                .chartBackground { _ in
+                    if let selectedWineType {
+                        VStack {
+                            Image(systemName: "wineglass.fill")
+                                .font(.largeTitle)
+                                .foregroundStyle(Color(selectedWineType.color))
+                            Text(selectedWineType.name)
+                                .font(.largeTitle)
+                            Text("In Stock: \(selectedWineType.inStock)")
+                        }
+                    } else {
+                        VStack {
+                            Image(systemName: "wineglass.fill")
+                                .font(.largeTitle)
+                                .foregroundColor(.red)
+                            Text("Select a segment")
+                        }
+                    }
+                }
                 .frame(height: 350)
+                if let selectedWineType {
+                    Text(selectedWineType.text)
+                }
                 Spacer()
+            }
+            .onChange(of: selectedCount) { oldValue, newValue in
+                if let newValue {
+//                    print(newValue)
+                    withAnimation {
+                        getSelectedWineType(value: newValue)
+                    }
+                }
             }
             .padding()
             .navigationTitle("Donut Chart")
+        }
+    }
+    private func getSelectedWineType(value: Int) {
+        var cumulativeTotal = 0
+        let wineType = wineTypes.first { wineType in
+            cumulativeTotal += wineType.inStock
+            if value <= cumulativeTotal {
+                selectedWineType = wineType
+                return true
+            }
+            return false
         }
     }
 }
